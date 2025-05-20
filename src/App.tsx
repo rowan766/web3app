9476a9714a2a0c56f2c6d4267fb3cdec6e3d7de7
张欢 <zhang_huan@cestc.cn>
2025-05-20 09:32:28 +0800

    增加代码审查组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 // src/App.tsx
-import AIChat from './components/AIChat/AIChat';
+// import AIChat from './components/AIChat/AIChat';
 
-import AgentPage from './components/agentPage/codeRevi'
+import AgentPage from './components/agentPage/codeRevi';
 
 function App() {
   return (
     <div className="App">
       {/* 使用默认的 DeepSeek 提供商 */}
       {/* <AIChat /> */}
-      
+
       {/* 或者明确指定提供商 */}
       {/* <AIChat provider="deepseek" /> */}
       <AgentPage></AgentPage>
-      
+
       {/* 或者提供自定义的消息处理函数 */}
       {/* <AIChat 
         onSendMessage={async (message) => {
